f7f4a0c6a61dd45e85dfafb9099a559b361735af
deborahjyu <djy3b@virginia.edu>
2022-05-26 17:17:41 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Link, Outlet} from "react-router-dom"
 import Banner from './components/banner';
 import { Button } from '@mui/material';
+import { Container } from '@mui/system';
 
 // add styling to the links 
 // embed the links differently 
@@ -11,35 +12,45 @@ function App() {
   return (
        <div className='App'>
          <Banner/>
+         <div className='container'>
+         <div className='navbar'>
           <Button component={Link} 
           to="/teacher-directory" 
           variant="outlined" 
-          color="primary">
+          style={{
+            borderRadius: 35,
+            backgroundColor: "#FFFFFF"}}>
           Teacher Directory</Button>
-
+          </div>
+          <div className='navbar'>
           <Button component={Link} 
           to="/student-directory" 
           variant="outlined" 
-          color="primary">
+          style={{
+            borderRadius: 35,
+            backgroundColor: "#FFFFFF"}}>
           Student Directory</Button>
-
-          <Button component={Link} 
-          to="/class-page" 
-          variant="outlined" 
-          color="primary">
-          Class Page</Button>
-
+          </div>
+          <div className='navbar'>
           <Button component={Link} 
           to="/school-calendar" 
           variant="outlined" 
-          color="primary">
+          color="primary"
+          style={{
+            borderRadius: 35,
+            backgroundColor: "#FFFFFF"}}>
           School Calendar</Button>
-
+          </div>
+          <div className='navbar'>
           <Button component={Link} 
           to="/class-dashboard" 
           variant="outlined" 
-          color="primary">
+          style={{
+            borderRadius: 35,
+            backgroundColor: "#FFFFFF"}}>
           Class Dashboard</Button>
+          </div>
+          </div>
         <Outlet/>
       </div>
    );
