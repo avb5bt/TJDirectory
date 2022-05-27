51a594a6a1c1cbe8e77eea7274a3a5b9ac6284cd
clara li <clara4695@gmail.com>
2022-05-27 11:14:29 -0400

    made changes

--- src/components/editTeacher.js
+++ src/components/editTeacher.js
@@ -66,14 +66,6 @@ function EditTeacher() {
         <div>
         <h2>Edit Teacher Directory</h2>
         <form onSubmit={changeTeacher} >
-            {/* <p>
-                <label>Last Name </label>
-                <input type="text" ref={lastFieldRef} required/>
-            </p>
-            <p>
-                <label>First Name </label>
-                <input type="text" ref={firstFieldRef} required/>
-            </p> */}
             <p>
               <TextField 
               required
@@ -100,10 +92,6 @@ function EditTeacher() {
                 />
               </MuiPickersUtilsProvider>
             </p>
-            {/* <p>
-                <label>Birthdate </label>
-                <input type="date" ref={birthFieldRef} required/>
-            </p>      */}
             <p>
               <FormControl required sx={{ m: 0.5, minWidth: 150 }}>
                 <InputLabel id="test-select-label">Gender</InputLabel>
@@ -118,14 +106,6 @@ function EditTeacher() {
                 </Select>
               </FormControl>
             </p>
-            {/* <p>
-                <label>Gender </label>
-                <select ref={genderFieldRef} required>
-                  <option value="Female">Female</option>
-                  <option value="Male">Male</option>
-                  <option value="Prefer not to say">Prefer not to say</option>
-                </select>
-            </p> */}
             <p>
               <FormControl required sx={{ m: 0.5, minWidth: 150 }}>
                 <InputLabel id="test-select-label">Grade Taught</InputLabel>
@@ -144,23 +124,11 @@ function EditTeacher() {
                 </Select>
               </FormControl>
             </p>
-            {/* <p>
-                <label>Grade Taught </label>
-                <select ref={gradeFieldRef} required>
-                  <option value="1st">1st</option>
-                  <option value="2nd">2nd</option>
-                  <option value="3rd">3rd</option>
-                  <option value="4th">4th</option>
-                  <option value="5th">5th</option>
-                  <option value="6th">6th</option>
-                </select>
-            </p> */}
             <Button
               type="submit"
               variant="outlined">
-              Add Teacher
+              Submit Changes
             </Button>
-                {/* <input type="submit"/> */}
         </form>
         </div>
     )
